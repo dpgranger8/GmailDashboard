94d47f0a21efed874ba8a53f63757cd8645d6ecf
David Granger <dpgranger8@gmail.com>
2025-01-27 07:32:55 -0700

    completed recent searches

--- app.js
+++ app.js
@@ -10,3 +10,43 @@ function focusedSearchBar() {
 function unFocusedSearchBar() {
     menu.style.display = "none";
 }
+
+const recentSearches = [
+    "expired coupon refund",
+    "exit strategy 2025",
+    "therapy appointment",
+    "investor feedback 'just make it simpler'",
+    "sugar-free gummy bears",
+    "re: just one more feature",
+];
+
+document.addEventListener('DOMContentLoaded', function () {
+    insertRecents();
+})
+
+
+function insertRecents() {
+    const recentsContainer = document.querySelector('.recents');
+    recentSearches.forEach(search => {
+        const recentDiv = document.createElement('div');
+        recentDiv.classList.add('recent');
+
+        const scheduleIcon = document.createElement('span');
+        scheduleIcon.classList.add('material-symbols-outlined');
+        scheduleIcon.textContent = 'schedule';
+
+        const recentText = document.createElement('span');
+        recentText.classList.add('recentText');
+        recentText.textContent = search;
+
+        const closeIcon = document.createElement('span');
+        closeIcon.classList.add('material-symbols-outlined');
+        closeIcon.textContent = 'close';
+
+        recentDiv.appendChild(scheduleIcon);
+        recentDiv.appendChild(recentText);
+        recentDiv.appendChild(closeIcon);
+
+        recentsContainer.appendChild(recentDiv);
+    });
+}
